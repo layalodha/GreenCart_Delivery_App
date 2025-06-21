b7557012961a6dd291ddf352b3317ea8890865c2
Laya Lodha <layalodha@gmail.com>
2025-06-21 12:47:58 +0530

    Added Changes to Origin

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 4000;
 await connectDB()
 await connectCloudinary()
 
-const allowedOrigins = ['http://localhost:5173', 'https://green-cart-delivery-app.vercel.app/']
+const allowedOrigins = ['http://localhost:5173', 'https://green-cart-delivery-app.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
